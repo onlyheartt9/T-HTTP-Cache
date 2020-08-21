763c33e2bfe95eed5e153c35693a414e40db856b
William <william@1centre.com>
2020-08-22 10:51:02 +1200

    格式优化

--- src/core/http-filter/bind.js
+++ src/core/http-filter/bind.js
@@ -3,8 +3,8 @@ import { http } from "./index";
 
 //对axios方法进行扩展封装并返回
 export function TBindAxios(axios) {
-    http.lib = axios;
-    http.type = "axios";
-    axios = registerAxiosFilter(axios);
-    return axios;
+  http.lib = axios;
+  http.type = "axios";
+  axios = registerAxiosFilter(axios);
+  return axios;
 }
--- src/core/http-filter/index.js
+++ src/core/http-filter/index.js
@@ -1,18 +1,18 @@
 import { getOptionByUrl } from "../instance/api";
 import { setHttpCache, getHttpCacheByKey, getCacheKey } from "../utils/index"
 export const http = {
-    lib: null,
-    type: "",
-    debug: false
+  lib: null,
+  type: "",
+  debug: false
 }
 export const urlLoading = [];
 
 
 export function debug(callback) {
-    if (http.debug) {
-        console.log("=========T-HTTP-CAHCE=========");
-        callback()
-    }
+  if (http.debug) {
+    console.log("=========T-HTTP-CAHCE=========");
+    callback()
+  }
 }
 
 /**
@@ -22,25 +22,25 @@ export function debug(callback) {
  * @param {*} cacheKeyOpt {type:请求类型,url:请求路径（get请求路径不带参数，需要去除）,params:参数}
  */
 export function commonRequestFilter(cacheKeyOpt) {
-    const { url, method } = cacheKeyOpt;
-    const option = getOptionByUrl(url, method);
-    const cacheKey = getCacheKey(cacheKeyOpt);
-    const response = getHttpCacheByKey(cacheKey);
-    if (!option) {
-        return "no option";
-    }
-    
-    //接口正在执行，不进行再次请求
-    if (urlLoading.includes(cacheKey)) {
-        return "loading";
-    } else if (!response) {
-        //没有缓存，请求接口
-        urlLoading.push(cacheKey);
-        return "normal";
-    } else {
-        //有缓存，返回缓存
-        return response;
-    }
+  const { url, method } = cacheKeyOpt;
+  const option = getOptionByUrl(url, method);
+  const cacheKey = getCacheKey(cacheKeyOpt);
+  const response = getHttpCacheByKey(cacheKey);
+  if (!option) {
+    return "no option";
+  }
+
+  //接口正在执行，不进行再次请求
+  if (urlLoading.includes(cacheKey)) {
+    return "loading";
+  } else if (!response) {
+    //没有缓存，请求接口
+    urlLoading.push(cacheKey);
+    return "normal";
+  } else {
+    //有缓存，返回缓存
+    return response;
+  }
 }
 /**
  * 统一响应拦截，
@@ -49,17 +49,17 @@ export function commonRequestFilter(cacheKeyOpt) {
  * @param {*} response 需要存储的返回值
  */
 export function commonResponseFilter(cacheKeyOpt, response) {
-    const { url } = cacheKeyOpt;
-    const opt = getOptionByUrl(url);
-    const cacheKey = getCacheKey(cacheKeyOpt);
-    if (opt && opt.dataFormat) {
-        response = opt.dataFormat(response);
-    }
-    if (urlLoading.includes(cacheKey)) {
-        setHttpCache(cacheKey, response);
-        urlLoading.splice(urlLoading.indexOf(cacheKey), 1);
-    };
-    return response;
+  const { url } = cacheKeyOpt;
+  const opt = getOptionByUrl(url);
+  const cacheKey = getCacheKey(cacheKeyOpt);
+  if (opt && opt.dataFormat) {
+    response = opt.dataFormat(response);
+  }
+  if (urlLoading.includes(cacheKey)) {
+    setHttpCache(cacheKey, response);
+    urlLoading.splice(urlLoading.indexOf(cacheKey), 1);
+  };
+  return response;
 }
 
 
